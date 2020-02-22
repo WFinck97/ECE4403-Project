c1a5c91bd71485c9092c2703a6a85c536d641dd4
Wesley Finck <wesleyfinck@gmail.com>
2020-02-22 16:31:13 -0400

    declared arraylist for absent and sub teachers

--- src/Driver.java
+++ src/Driver.java
@@ -1,11 +1,16 @@
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.ArrayList;
 
 // The "*" tells Java to import all of the Classes included in the library.
 import org.apache.commons.csv.*;
 
-public class Driver {
 
+public class Driver {
+	
+	private ArrayList<AbsentTeacher> absentTeachers;
+	private ArrayList<SubstituteTeacher> substituteTeachers;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
