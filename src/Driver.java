86c869e3f0f35a2677b9d65b91ef3a361b751b31
Wesley Finck <wesleyfinck@gmail.com>
2020-02-22 16:19:50 -0400

    created initial classes

---
+++ src/Driver.java
@@ -0,0 +1,14 @@
+import java.io.FileReader;
+import java.io.FileWriter;
+
+// The "*" tells Java to import all of the Classes included in the library.
+import org.apache.commons.csv.*;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
